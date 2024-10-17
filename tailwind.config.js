3edc879fb61ddf95c545d5b133f95167021cf50b
Kagayaku Kagami <jianyao2555@gmail.com>
2024-10-17 15:38:36 +0700

    Setup custom tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: 'hx-',
+  content: [
+    "./layouts/**/*.html",
+    "./content/**/*.md"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
